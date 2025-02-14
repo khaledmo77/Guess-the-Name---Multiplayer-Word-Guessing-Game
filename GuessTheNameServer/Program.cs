22df8af3839bfe9e67036721e05a7b7f2446b07f
khaledmo77 <khaledmo77@outlook.com>
2025-02-14 23:23:35 +0200

    Created full project structure

--- GuessTheNameServer/Program.cs
+++ GuessTheNameServer/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace GuessTheNameServer
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+"" 
